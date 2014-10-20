6df0688c6ff0367f320b24e96a671d6523f25e9a
Karen Piotrowski <piotrowski.karen@gmail.com>
2014-10-20 18:29:08 -0430

    Fue creado el proyecto ProyectoSCRUM

---
+++ src/main/java/com/mycompany/uniendosparkmongo/ServicioBD.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.uniendosparkmongo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.WriteResult;
+import com.mongodb.util.JSON;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Servicio de la BD con mongo en el cual (por ahora) podemos crear/insertar
+ * proyectos o listarlos, para ello debemos conectarnos a mongo, crear la BD,
+ * sus colecciones para poder realizar las operaciones.
+ *
+ * @author karen
+ */
+public class ServicioBD {
+
+    MongoClient mongo;
+    DB db;
+    DBCollection proyecto;
+
+    public ServicioBD() {
+
+        try {
+            // Se conecta a mongoDB (Connect to MongoDB)
+            mongo = new MongoClient("localhost", 27017);
+
+            // Crear la base de datos (si no existe) (Get database)
+            db = mongo.getDB("pruebaConSpark");
+
+            // Se crea la coleccion "proyecto"
+            proyecto = db.getCollection("proyecto");
+
+            System.out.println("Connecting to MongoDB@" + mongo.getAllAddress());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /* Se crea un documento (BasicDBObject) con un nombre y descripcion y se inserta
+     en la coleccion proyecto.
+     */
+    public BasicDBObject crearProyecto(JSONObject proy) {
+        
+        
+
+        BasicDBObject doc = new BasicDBObject("nombre", proy.get("nombre"))
+                .append("descripcion", proy.get("descripcion"));
+
+        WriteResult result = proyecto.insert(doc);
+
+        return doc;
+    }
+
+    /* Coloca en una lista todos los proyectos que estan en la BD, iterando
+       sobre la coleccion con un cursor para tomar cada documento de ella.
+     */
+    @SuppressWarnings("unchecked")
+    public JSONArray leerTodosProyectos() {
+        DBCursor cursor = proyecto.find();
+        JSONArray resultados = new JSONArray();
+
+        try {
+            while (cursor.hasNext()) {
+                JSONObject doc = new JSONObject(JSON.serialize(cursor.next()));
+                String clean_id = (doc.getJSONObject("_id").get("$oid")).toString();
+                doc.put("_id", clean_id);
+
+                resultados.put(doc);
+            }
+        } finally {
+            cursor.close();
+        }
+        return resultados;
+    }
+
+}
---
+++ src/main/java/com/mycompany/uniendosparkmongo/Spark.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.uniendosparkmongo;
+
+import com.mongodb.util.JSON;
+import java.net.UnknownHostException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+/**
+ *
+ * @author karen
+ */
+public class Spark {
+
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        final ServicioBD servicioBDProyecto = new ServicioBD();
+
+        
+        /* Aqui nos muestra un mensaje si en la BD no hay proyectos o nos muestra
+           una lista con los nombres de los proyectos.
+           Correr y poner en postman: http://localhost:4567/listarproyectos
+        */
+        get(new Route("/listarproyectos") {
+            @Override
+            public Object handle(Request request, Response response) {
+
+                /* Creamos una lista de proyectos como un arreglo de JSON
+                   en la cual se agregan todos que hay en la BD con el 
+                   metodo "leerTodosProyectos"
+                */
+
+                JSONArray proyectos = servicioBDProyecto.leerTodosProyectos();
+                return proyectos;
+            }
+        });
+
+        /* Se agrega en la bd un proyecto con los parametros que son pasados por url
+        */
+        post(new Route("/crearproyecto") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String nombre = request.queryParams("nombre");
+                String descripcion = request.queryParams("descripcion");
+                JSONObject doc;
+
+                JSONObject proy = new JSONObject();
+                proy.put("nombre", nombre);
+                proy.put("descripcion", descripcion);
+                
+                doc = new JSONObject(JSON.serialize(servicioBDProyecto.crearProyecto(proy)));
+                String clean_id = (doc.getJSONObject("_id").get("$oid")).toString();
+                doc.put("_id", clean_id);
+                
+                return doc;
+            }
+        });
+    }
+
+}

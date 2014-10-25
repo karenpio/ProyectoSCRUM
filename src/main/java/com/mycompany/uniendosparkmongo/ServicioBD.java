7f3886b75d6be7eafc0c8f78a56984fa744e700d
Karen Piotrowski <piotrowski.karen@gmail.com>
2014-10-25 12:55:35 -0430

    Se tiene el servicio put de /asociarparticipante

--- src/main/java/com/mycompany/uniendosparkmongo/ServicioBD.java
+++ src/main/java/com/mycompany/uniendosparkmongo/ServicioBD.java
@@ -9,6 +9,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.WriteResult;
 import com.mongodb.util.JSON;
@@ -28,6 +29,7 @@ public class ServicioBD {
     DB db;
     DBCollection proyecto;
     DBCollection requisito;
+    DBCollection participante;
 
     public ServicioBD() {
 
@@ -40,9 +42,12 @@ public class ServicioBD {
 
             // Se crea la coleccion "proyecto"
             proyecto = db.getCollection("proyecto");
-            
+
             // Se crea la coleccion "requisito"
             requisito = db.getCollection("requisito");
+
+            // Se crea la coleccion "participante"
+            participante = db.getCollection("participante");
 
             System.out.println("Connecting to MongoDB@" + mongo.getAllAddress());
 
@@ -55,35 +60,81 @@ public class ServicioBD {
      en la coleccion proyecto.
      */
     public BasicDBObject crearProyecto(JSONObject proy) {
-        
 
         BasicDBObject doc = new BasicDBObject("nombre", proy.get("nombre"))
                 .append("descripcion", proy.get("descripcion"));
-        
-        
 
-        WriteResult result = proyecto.insert(doc);
+        proyecto.insert(doc);
 
         return doc;
     }
     
-    
-    /* 
-        Funcion utilizada para asegurar la unicidad del nombre del proyecto
-        Retorna true si ya existe un proyecto con el nombre dado
-    */
-    public Boolean estaProyectoNombre(String nombreProy){
-        BasicDBObject query = new BasicDBObject("nombre", new BasicDBObject("$eq", nombreProy));
-        DBCursor cursor = proyecto.find(query);
-        System.out.println(cursor.itcount());
-        
-        return cursor.count() != 0;
+    public BasicDBObject actualizarProyecto(JSONObject proy) {
 
+        DBCursor proyCursor = estaProyectoNombre(proy.get("nombre").toString());
+        BasicDBObject proyect = (BasicDBObject) proyCursor.next();
+        
+        Object o = JSON.parse(proy.toString());
+        BasicDBObject proyActualizado = (BasicDBObject) o;
+
+        
+        proyecto.save(proyActualizado);
+
+        //proyecto.update(proyect, proyActualizado);
+
+        return proyActualizado;
     }
     
+    
 
+
+    /* 
+     Funcion utilizada para asegurar la unicidad del nombre del proyecto
+     Retorna true si ya existe un proyecto con el nombre dado
+     */
+    public DBCursor estaProyectoNombre(String nombreProy) {
+        BasicDBObject query = new BasicDBObject("nombre", new BasicDBObject("$eq", nombreProy));
+        DBCursor cursor = proyecto.find(query);
+
+        return cursor;
+
+    }
+
+    /* Se crea un documento (BasicDBObject) con email, nombre y telefono y se inserta
+     en la coleccion participante.
+     */
+    public BasicDBObject crearParticipante(JSONObject part) {
+
+        BasicDBObject doc = new BasicDBObject("email", part.get("email"))
+                .append("nombre", part.get("nombre")).append("telefono", part.get("telefono"));
+
+        participante.insert(doc);
+
+        return doc;
+    }
+
+    /* 
+     Unicidad del email del participante  
+     */
+    public DBCursor estaEmailParticipante(String emailPart) {
+        BasicDBObject query = new BasicDBObject("email", new BasicDBObject("$eq", emailPart));
+        DBCursor cursor = participante.find(query);
+
+        return cursor;
+
+    }
+
+    public JSONObject limpiarID(BasicDBObject obj) {
+        JSONObject doc = new JSONObject(JSON.serialize(obj));
+        String clean_idPart = (doc.getJSONObject("_id").get("$oid")).toString();
+        doc.put("_id", clean_idPart);
+        
+        return doc;
+    }
+
+    //public BasicDBObject buscarProyecto(String nombre){}
     /* Coloca en una lista todos los proyectos que estan en la BD, iterando
-       sobre la coleccion con un cursor para tomar cada documento de ella.
+     sobre la coleccion con un cursor para tomar cada documento de ella.
      */
     @SuppressWarnings("unchecked")
     public JSONArray leerTodosProyectos() {
--- src/main/java/com/mycompany/uniendosparkmongo/Spark.java
+++ src/main/java/com/mycompany/uniendosparkmongo/Spark.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.uniendosparkmongo;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBRef;
 import com.mongodb.util.JSON;
 import java.net.UnknownHostException;
 import org.json.JSONArray;
@@ -14,6 +18,7 @@ import spark.Response;
 import spark.Route;
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.put;
 
 /**
  *
@@ -55,7 +60,7 @@ public class Spark {
                 JSONObject proy = new JSONObject();
 
                 // Si el nombre del proyecto no existe, entonces se agrega
-                if (!servicioBDProyecto.estaProyectoNombre(nombre)) {
+                if (servicioBDProyecto.estaProyectoNombre(nombre).count() == 0) {
                     proy.put("nombre", nombre);
                     proy.put("descripcion", descripcion);
 
@@ -67,6 +72,68 @@ public class Spark {
                 return doc;
             }
         });
+
+        /*
+            Dado un nombre de proyecto y un email de participante, se actualiza el proyecto
+            agregando al participante a la lista de participantes (si la tiene) o crea una nueva 
+            lista de participantes con el correo dado.
+        */
+        put(new Route("/asociarparticipante") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String nombreProy = request.queryParams("nombreProyecto");
+                String email = request.queryParams("email"); 
+                
+                // Lamamos a la funcion estaProyectoNombre para tomar el proyecto que queremos
+                DBCursor cursorProy = servicioBDProyecto.estaProyectoNombre(nombreProy);
+             
+                // Tomamos el proyecto al cual le agregaremos el participante
+                BasicDBObject proy = (BasicDBObject) cursorProy.next();
+                
+                // Llamamos a la funcion estaEmailParticipante para tomar el participante que queremos
+                DBCursor cursorPart = servicioBDProyecto.estaEmailParticipante(email);
+
+                // Tomamos el proyecto al cual le agregaremos el participante
+                BasicDBObject part = (BasicDBObject) cursorPart.next();
+                
+                BasicDBList lista;
+                
+                
+                if (proy.get("participantes") != null) {
+                    lista = (BasicDBList) proy.get("participantes");
+
+                } else {
+                    lista = new BasicDBList();   
+                }
+                
+                
+                /*
+                    Verificar que el participante no haya sido agregado antes.
+                    Se utiliza el email en vez del _id porque agregaba mucha basura,
+                    esto debemos acomodarlo mas adelante y preguntarle a Ascander.
+                */
+                        
+                if (!lista.contains(part.get("email"))){
+                    lista.add(part.get("email"));
+                }
+ 
+                // Por ahora limpia el _id, pero no estamos seguros si esto sea lo mas conveniente.
+                JSONObject proyDoc = servicioBDProyecto.limpiarID(proy);
+                //JSONObject proyDoc = new JSONObject(JSON.serialize(proy));
+                
+                // Colocamos la lista actualizada en el JSON
+                proyDoc.put("participantes", lista);
+                
+                // Actualizamos la base de datos dado el JSON actualizado
+                BasicDBObject actualizacion = servicioBDProyecto.actualizarProyecto(proyDoc);
+
+                return actualizacion;
+            }
+        });
+
+        
+        
     }
+    
 
 }

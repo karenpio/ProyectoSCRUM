94c021b6984c2c07c4fee0139c1b73da0bbeb39b
Yeiker Vazquez <yeikervazmol@gmail.com>
2014-10-25 16:35:51 -0430

    Mejor estructura del proyecto.

--- src/main/java/backend/mongo/ServicioBD.java
+++ src/main/java/backend/mongo/ServicioBD.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.uniendosparkmongo;
+package backend.mongo;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
--- src/main/java/backend/mongo/Spark.java
+++ src/main/java/backend/mongo/Spark.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.uniendosparkmongo;
+package backend.mongo;
 
 import java.net.UnknownHostException;
 import org.json.JSONArray;
